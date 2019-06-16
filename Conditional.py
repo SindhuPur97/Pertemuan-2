49aa43f4d08fb1df875e0a3439ab2a8f93bf2aec
SindhuPurnamaSantoso <44023418+SindhuPur97@users.noreply.github.com>
2019-06-16 11:03:21 +0700

    Create Conditional.py

---
+++ Conditional.py
@@ -0,0 +1,10 @@
+x = input("Masukan nilai pertama : ")
+y = input("Masukan nilai kedua : ")
+
+print("\nNilai pertama : ",x)
+print("Nilai kedua   : ",y)
+
+if(x>y):
+	print("\nNilai pertama lebih besar daripada nilai kedua",x,">",y)
+else:
+	print("\nNilai pertama lebih kecil daripada nilai kedua",x,"<",y)
